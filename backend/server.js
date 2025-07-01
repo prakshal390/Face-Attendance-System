ad61426a182ecc64d6ac2f63ecc1a00f2d872484
prakshal390 <sj8104669@gmail.com>
2025-07-01 16:19:12 +0530

    proper working without deploy

--- backend/server.js
+++ backend/server.js
@@ -18,33 +18,6 @@
 
 
 
-// const express = require("express");
-// const cors = require("cors");
-// const dotenv = require("dotenv");
-// const connectDB = require("./config/db");
-
-// dotenv.config();
-// connectDB();
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// // ✅ Routes
-// app.use("/api/users", require("./routes/userRoutes"));
-// app.use("/api/attendance", require("./routes/attendanceRoutes"));
-
-// // ✅ Root route for testing server status
-// app.get("/", (req, res) => {
-//   res.send("✅ Face Attendance API is running...");
-// });
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-
-
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -54,25 +27,20 @@ dotenv.config();
 connectDB();
 
 const app = express();
-
-app.use(
-  cors({
-    origin: "https://face-attendance-systemm.vercel.app", // ✅ your frontend Vercel URL
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
-
+app.use(cors());
 app.use(express.json());
 
 // ✅ Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/attendance", require("./routes/attendanceRoutes"));
 
-// ✅ Root route for testing
+// ✅ Root route for testing server status
 app.get("/", (req, res) => {
   res.send("✅ Face Attendance API is running...");
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
